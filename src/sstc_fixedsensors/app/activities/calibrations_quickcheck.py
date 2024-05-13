387b56a8438354bd0d64ea0301da1f10dc57f156
Jobelund <124563223+jobelund@users.noreply.github.com>
2024-05-13 11:28:13 +0200

    loading calibration file

--- src/sstc_fixedsensors/app/activities/calibrations_quickcheck.py
+++ src/sstc_fixedsensors/app/activities/calibrations_quickcheck.py
@@ -2,20 +2,47 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+
+def sstc_read_csv(uploaded_file, header= 1,  delete_rows=[]):
+ 
+    data = pd.read_csv(
+        uploaded_file, 
+        delimiter=',',
+        decimal='.',
+        header=header,
+        encoding='utf-8',
+        infer_datetime_format=True,             
+        )
+    
+    _to_delete = []
+
+    for i in data.index:
+        if int(i) in delete_rows:
+            _to_delete.append(i)
+    
+    data.drop(_to_delete, inplace=True)
+
+    return data
+
+
+col1, col2 = st.columns([1,1])
+
+with col1:
+    uploaded_file = st.file_uploader(
+        label="Choose a calibration file:",
+        type=["dat", "csv", "txt", "tsv"])
+    
+with col2:
+    delete_rows = st.multiselect(
+        label='metadata rows to delete:',
+        options=[0,1,2,3,4],
+        )
+
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    
+    df = sstc_read_csv(
+        uploaded_file,
+        header=1, 
+        delete_rows=delete_rows)
+    
+    st.write(df)
