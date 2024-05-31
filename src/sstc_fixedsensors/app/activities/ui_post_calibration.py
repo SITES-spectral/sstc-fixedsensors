80b40100e694f9cd81f564ab95d9dda42bcbca8a
Jobelund <124563223+jobelund@users.noreply.github.com>
2024-05-31 14:18:31 +0200

    wip

--- src/sstc_fixedsensors/app/activities/ui_post_calibration.py
+++ src/sstc_fixedsensors/app/activities/ui_post_calibration.py
@@ -1,8 +1,4 @@
 import streamlit as st
-from sstc_utils.utils import fetch_markdown_instructions_from_github
 
 
-
-def load_instructions():
-    instructions_md = fetch_markdown_instructions_from_github(url='instructions.md')
-    st.markdown(instructions_md)
+st.info(__file__)
